e070628b4fcdcb1a9bbe7a213d06cac41288fbec
Rich Caudle <richard@rcaudle.co.uk>
2013-07-18 11:46:23 +0100

    Changes to config

--- server.rb
+++ server.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'sinatra/base'
 require 'yaml'
-require 'pusher'
 
 class Server < Sinatra::Base
 
@@ -9,15 +8,9 @@ class Server < Sinatra::Base
 	set :public_folder, Proc.new { File.join(root, "public") }
 	config = YAML.load_file('./config.yml')
 
-	Pusher.app_id = config['pusher']['app_id']
-	Pusher.key = config['pusher']['app_key']
-	Pusher.secret = config['pusher']['app_secret']
-
 	# main view action
 	get '/' do
-		#@app_key = config['pusher']['app_key']
-
-		puts Pusher.get('/channels')
+		@app_key = config['pusher']['app_key']
 
 		erb :index
 	end
